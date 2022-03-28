b45f7bbd5711416084aae7221808f54f286d6bd7
Eleftherios Lymperopoulos <64773191+LefterisLymp@users.noreply.github.com>
2022-03-28 21:34:30 +0300

    Added queries

---
+++ queries/scenarios-2-3-4.sql
@@ -0,0 +1,153 @@
+/*Query 1*/
+WITH customer_total_return 
+     AS (SELECT sr_customer_sk     AS ctr_customer_sk, 
+                sr_store_sk        AS ctr_store_sk, 
+                Sum(sr_return_amt) AS ctr_total_return 
+         FROM   store_returns, 
+                date_dim 
+         WHERE  sr_returned_date_sk = d_date_sk 
+                AND d_year = 2001 
+         GROUP  BY sr_customer_sk, 
+                   sr_store_sk) 
+SELECT c_customer_id 
+FROM   customer_total_return ctr1, 
+       store, 
+       customer 
+WHERE  ctr1.ctr_total_return > (SELECT Avg(ctr_total_return) * 1.2 
+                                FROM   customer_total_return ctr2 
+                                WHERE  ctr1.ctr_store_sk = ctr2.ctr_store_sk) 
+       AND s_store_sk = ctr1.ctr_store_sk 
+       AND s_state = 'TN' 
+       AND ctr1.ctr_customer_sk = c_customer_sk 
+ORDER  BY c_customer_id
+LIMIT 100;
+
+
+
+/*Query 7*/
+SELECT i_item_id, 
+               Avg(ss_quantity)    agg1, 
+               Avg(ss_list_price)  agg2, 
+               Avg(ss_coupon_amt)  agg3, 
+               Avg(ss_sales_price) agg4 
+FROM   store_sales, 
+       customer_demographics, 
+       date_dim, 
+       item, 
+       promotion 
+WHERE  ss_sold_date_sk = d_date_sk 
+       AND ss_item_sk = i_item_sk 
+       AND ss_cdemo_sk = cd_demo_sk 
+       AND ss_promo_sk = p_promo_sk 
+       AND cd_gender = 'F' 
+       AND cd_marital_status = 'W' 
+       AND cd_education_status = '2 yr Degree' 
+       AND ( p_channel_email = 'N' 
+              OR p_channel_event = 'N' ) 
+       AND d_year = 1998 
+GROUP  BY i_item_id 
+ORDER  BY i_item_id
+LIMIT 100;
+
+
+
+/*Query 12*/
+SELECT
+         i_item_id , 
+         i_item_desc , 
+         i_category , 
+         i_class , 
+         i_current_price , 
+         Sum(ws_ext_sales_price)                                                              AS itemrevenue ,
+         Sum(ws_ext_sales_price)*100/Sum(Sum(ws_ext_sales_price)) OVER (partition BY i_class) AS revenueratio
+FROM     web_sales , 
+         item , 
+         date_dim 
+WHERE    ws_item_sk = i_item_sk 
+AND      i_category IN ('Home', 
+                        'Men', 
+                        'Women') 
+AND      ws_sold_date_sk = d_date_sk 
+AND      d_date BETWEEN Cast('2000-05-11' AS DATE) AND      ( 
+                  Cast('2000-05-11' AS DATE) + INTERVAL '30' day) 
+GROUP BY i_item_id , 
+         i_item_desc , 
+         i_category , 
+         i_class , 
+         i_current_price 
+ORDER BY i_category , 
+         i_class , 
+         i_item_id , 
+         i_item_desc , 
+         revenueratio 
+LIMIT 100;
+
+
+
+/*Query 15*/
+SELECT ca_zip, 
+               Sum(cs_sales_price) 
+FROM   catalog_sales, 
+       customer, 
+       customer_address, 
+       date_dim 
+WHERE  cs_bill_customer_sk = c_customer_sk 
+       AND c_current_addr_sk = ca_address_sk 
+       AND ( Substr(ca_zip, 1, 5) IN ( '85669', '86197', '88274', '83405', 
+                                       '86475', '85392', '85460', '80348', 
+                                       '81792' ) 
+              OR ca_state IN ( 'CA', 'WA', 'GA' ) 
+              OR cs_sales_price > 500 ) 
+       AND cs_sold_date_sk = d_date_sk 
+       AND d_qoy = 1 
+       AND d_year = 1998 
+GROUP  BY ca_zip 
+ORDER  BY ca_zip
+LIMIT 100;
+
+
+/*Query 62*/
+SELECT Substr(w_warehouse_name, 1, 20), 
+               sm_type, 
+               web_name, 
+               Sum(CASE 
+                     WHEN ( ws_ship_date_sk - ws_sold_date_sk <= 30 ) THEN 1 
+                     ELSE 0 
+                   END) AS "30 days", 
+               Sum(CASE 
+                     WHEN ( ws_ship_date_sk - ws_sold_date_sk > 30 ) 
+                          AND ( ws_ship_date_sk - ws_sold_date_sk <= 60 ) THEN 1 
+                     ELSE 0 
+                   END) AS "31-60 days", 
+               Sum(CASE 
+                     WHEN ( ws_ship_date_sk - ws_sold_date_sk > 60 ) 
+                          AND ( ws_ship_date_sk - ws_sold_date_sk <= 90 ) THEN 1 
+                     ELSE 0 
+                   END) AS "61-90 days", 
+               Sum(CASE 
+                     WHEN ( ws_ship_date_sk - ws_sold_date_sk > 90 ) 
+                          AND ( ws_ship_date_sk - ws_sold_date_sk <= 120 ) THEN 
+                     1 
+                     ELSE 0 
+                   END) AS "91-120 days", 
+               Sum(CASE 
+                     WHEN ( ws_ship_date_sk - ws_sold_date_sk > 120 ) THEN 1 
+                     ELSE 0 
+                   END) AS ">120 days" 
+FROM   web_sales, 
+       warehouse, 
+       ship_mode, 
+       web_site, 
+       date_dim 
+WHERE  d_month_seq BETWEEN 1222 AND 1222 + 11 
+       AND ws_ship_date_sk = d_date_sk 
+       AND ws_warehouse_sk = w_warehouse_sk 
+       AND ws_ship_mode_sk = sm_ship_mode_sk 
+       AND ws_web_site_sk = web_site_sk 
+GROUP  BY Substr(w_warehouse_name, 1, 20), 
+          sm_type, 
+          web_name 
+ORDER  BY Substr(w_warehouse_name, 1, 20), 
+          sm_type, 
+          web_name
+LIMIT 100;
